e3d3608d52c2944821a68e9a7f21242eb953ea92
egor <egor.neon@gmail.com>
2019-07-11 12:01:03 +0200

    Fix querystring

--- IdentitySdkCore/IdentitySdkCore/Classes/ReachFiveApi.swift
+++ IdentitySdkCore/IdentitySdkCore/Classes/ReachFiveApi.swift
@@ -24,7 +24,7 @@ public class ReachFiveApi {
     
     public func loginWithProvider(loginProviderRequest: LoginProviderRequest, callback: @escaping Callback<AccessTokenResponse, ReachFiveError>) {
         Alamofire
-            .request(createUrl(path: "/identity/v1/oauth/provider/token&device=\(deviceInfo)"), method: .post, parameters: loginProviderRequest.toJSON(), encoding: JSONEncoding.default)
+            .request(createUrl(path: "/identity/v1/oauth/provider/token?device=\(deviceInfo)"), method: .post, parameters: loginProviderRequest.toJSON(), encoding: JSONEncoding.default)
             .validate(statusCode: 200..<300)
             .validate(contentType: ["application/json"])
             .responseObject(completionHandler: handleResponse(callback: callback))
@@ -32,7 +32,7 @@ public class ReachFiveApi {
     
     public func signupWithPassword(signupRequest: SignupRequest, callback: @escaping Callback<AccessTokenResponse, ReachFiveError>) {
         Alamofire
-            .request(createUrl(path: "/identity/v1/signup-token&device=\(deviceInfo)"), method: .post, parameters: signupRequest.toJSON(), encoding: JSONEncoding.default)
+            .request(createUrl(path: "/identity/v1/signup-token?device=\(deviceInfo)"), method: .post, parameters: signupRequest.toJSON(), encoding: JSONEncoding.default)
             .validate(statusCode: 200..<300)
             .validate(contentType: ["application/json"])
             .responseObject(completionHandler: handleResponse(callback: callback))
@@ -40,7 +40,7 @@ public class ReachFiveApi {
     
     public func loginWithPassword(loginRequest: LoginRequest, callback: @escaping Callback<AccessTokenResponse, ReachFiveError>) {
         Alamofire
-            .request(createUrl(path: "/oauth/token"), method: .post, parameters: loginRequest.toJSON(), encoding: JSONEncoding.default)
+            .request(createUrl(path: "/oauth/token?device=\(deviceInfo)"), method: .post, parameters: loginRequest.toJSON(), encoding: JSONEncoding.default)
             .validate(statusCode: 200..<300)
             .validate(contentType: ["application/json"])
             .responseObject(completionHandler: handleResponse(callback: callback))
@@ -48,7 +48,7 @@ public class ReachFiveApi {
     
     public func authWithCode(authCodeRequest: AuthCodeRequest, callback: @escaping Callback<AccessTokenResponse, ReachFiveError>) {
         Alamofire
-            .request(createUrl(path: "/oauth/token"), method: .post, parameters: authCodeRequest.toJSON(), encoding: JSONEncoding.default)
+            .request(createUrl(path: "/oauth/token?device=\(deviceInfo)"), method: .post, parameters: authCodeRequest.toJSON(), encoding: JSONEncoding.default)
             .validate(statusCode: 200..<300)
             .validate(contentType: ["application/json"])
             .responseObject(completionHandler: handleResponse(callback: callback))
@@ -57,7 +57,7 @@ public class ReachFiveApi {
     public func verifyPhoneNumber(authToken: AuthToken, verifyPhoneNumberRequest: VerifyPhoneNumberRequest, callback: @escaping Callback<Void, ReachFiveError>) {
         Alamofire
             .request(
-                createUrl(path: "/identity/v1/verify-phone-number&device=\(deviceInfo)"),
+                createUrl(path: "/identity/v1/verify-phone-number?device=\(deviceInfo)"),
                 method: .post,
                 parameters: verifyPhoneNumberRequest.toJSON(),
                 encoding: JSONEncoding.default,
@@ -71,7 +71,7 @@ public class ReachFiveApi {
     public func updateEmail(authToken: AuthToken, updateEmailRequest: UpdateEmailRequest, callback: @escaping Callback<Profile, ReachFiveError>) {
         Alamofire
             .request(
-                createUrl(path: "/identity/v1/update-email&device=\(deviceInfo)"),
+                createUrl(path: "/identity/v1/update-email?device=\(deviceInfo)"),
                 method: .post,
                 parameters: updateEmailRequest.toJSON(),
                 encoding: JSONEncoding.default,
@@ -89,7 +89,7 @@ public class ReachFiveApi {
     ) {
         Alamofire
             .request(
-                createUrl(path: "/identity/v1/update-profile&device=\(deviceInfo)"),
+                createUrl(path: "/identity/v1/update-profile?device=\(deviceInfo)"),
                 method: .post,
                 parameters: profile.toJSON(),
                 encoding: JSONEncoding.default,
@@ -107,7 +107,7 @@ public class ReachFiveApi {
     ) {
         Alamofire
             .request(createUrl(
-                path: "/identity/v1/forgot-password&device=\(deviceInfo)"),
+                path: "/identity/v1/forgot-password?device=\(deviceInfo)"),
                 method: .post,
                 parameters: requestPasswordResetRequest.toJSON(),
                 encoding: JSONEncoding.default,
