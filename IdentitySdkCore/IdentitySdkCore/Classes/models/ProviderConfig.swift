0e28d72e99213f91f4ec2e4b49374ad0ea429fdd
Guillaume Partenet <guillaume.partenet@gmail.com>
2020-05-14 08:57:36 +0200

    fix: remove unused config value

--- IdentitySdkCore/IdentitySdkCore/Classes/models/ProviderConfig.swift
+++ IdentitySdkCore/IdentitySdkCore/Classes/models/ProviderConfig.swift
@@ -3,6 +3,5 @@ import Foundation
 public class ProviderConfig: Codable {
     public let provider: String
     public let clientId: String?
-    public let clientSecret: String?
     public let scope: [String]?
 }
