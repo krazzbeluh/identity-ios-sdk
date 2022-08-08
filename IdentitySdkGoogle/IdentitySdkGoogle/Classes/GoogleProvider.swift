fee6f4190130d682d475c55670511fdb8d2f5ad8
François Devémy <61689139+FrancoisDevemy5@users.noreply.github.com>
2022-08-08 18:58:31 +0200

    [CA-1532] Update Google Sign In to version 6 (#105)

--- IdentitySdkGoogle/IdentitySdkGoogle/Classes/GoogleProvider.swift
+++ IdentitySdkGoogle/IdentitySdkGoogle/Classes/GoogleProvider.swift
@@ -26,17 +26,13 @@ public class GoogleProvider: ProviderCreator {
     }
 }
 
-public class ConfiguredGoogleProvider: NSObject, Provider, GIDSignInDelegate {
+public class ConfiguredGoogleProvider: NSObject, Provider {
     public var name: String = GoogleProvider.NAME
     
     var sdkConfig: SdkConfig
     var providerConfig: ProviderConfig
     var reachFiveApi: ReachFiveApi
     var clientConfigResponse: ClientConfigResponse
-    
-    var scope: [String]? = []
-    var origin: String = ""
-    var promise: Promise<AuthToken, ReachFiveError>?
     
     public init(sdkConfig: SdkConfig, providerConfig: ProviderConfig, reachFiveApi: ReachFiveApi, clientConfigResponse: ClientConfigResponse) {
         self.sdkConfig = sdkConfig
@@ -45,50 +41,45 @@ public class ConfiguredGoogleProvider: NSObject, Provider, GIDSignInDelegate {
         self.clientConfigResponse = clientConfigResponse
     }
     
-    public func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        if error != nil {
-            promise?.failure(.AuthFailure(reason: error.localizedDescription))
-        } else {
-            let loginProviderRequest = LoginProviderRequest(
-                provider: providerConfig.provider,
-                providerToken: user.authentication.accessToken,
-                code: nil,
-                origin: origin,
-                clientId: sdkConfig.clientId,
-                responseType: "token",
-                scope: scope != nil ? scope!.joined(separator: " ") : clientConfigResponse.scope
-            )
-            reachFiveApi
-                .loginWithProvider(loginProviderRequest: loginProviderRequest)
-                .flatMap({ AuthToken.fromOpenIdTokenResponseFuture($0) })
-                .onSuccess { authToken in
-                    self.promise?.success(authToken)
-                }
-                .onFailure { error in
-                    self.promise?.failure(error)
-                }
-        }
-    }
-    
     public func login(
         scope: [String]?,
         origin: String,
         viewController: UIViewController?
     ) -> Future<AuthToken, ReachFiveError> {
-        self.scope = scope
-        self.origin = origin
         let promise = Promise<AuthToken, ReachFiveError>()
-        self.promise = promise
-        GIDSignIn.sharedInstance().clientID = providerConfig.clientId
-        GIDSignIn.sharedInstance().scopes = providerConfig.scope
-        GIDSignIn.sharedInstance().delegate = self
-        GIDSignIn.sharedInstance().presentingViewController = viewController
-        GIDSignIn.sharedInstance().signIn()
+        guard let viewController = viewController else {
+            promise.failure(.TechnicalError(reason: "No presenting viewController"))
+            return promise.future
+        }
+        
+        let configuration = GIDConfiguration(clientID: providerConfig.clientId!)
+        GIDSignIn.sharedInstance.signIn(with: configuration, presenting: viewController, hint: nil, additionalScopes: providerConfig.scope) { user, error in
+            guard let user = user else {
+                let reason = error == nil ? "No user" : error!.localizedDescription
+                promise.failure(.AuthFailure(reason: reason))
+                return
+            }
+            
+            let loginProviderRequest = LoginProviderRequest(
+                provider: self.providerConfig.provider,
+                providerToken: user.authentication.accessToken,
+                code: nil,
+                origin: origin,
+                clientId: self.sdkConfig.clientId,
+                responseType: "token",
+                scope: scope != nil ? scope!.joined(separator: " ") : self.clientConfigResponse.scope
+            )
+            promise.completeWith(
+                self.reachFiveApi
+                    .loginWithProvider(loginProviderRequest: loginProviderRequest)
+                    .flatMap({ AuthToken.fromOpenIdTokenResponseFuture($0) })
+            )
+        }
         return promise.future
     }
     
     public func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any]) -> Bool {
-        GIDSignIn.sharedInstance().handle(url)
+        GIDSignIn.sharedInstance.handle(url)
     }
     
     public func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
@@ -96,7 +87,7 @@ public class ConfiguredGoogleProvider: NSObject, Provider, GIDSignInDelegate {
             UIApplication.OpenURLOptionsKey.sourceApplication.rawValue: sourceApplication as AnyObject,
             UIApplication.OpenURLOptionsKey.annotation.rawValue: annotation as AnyObject
         ]
-        return GIDSignIn.sharedInstance().handle(url)
+        return GIDSignIn.sharedInstance.handle(url)
     }
     
     public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -106,8 +97,8 @@ public class ConfiguredGoogleProvider: NSObject, Provider, GIDSignInDelegate {
     public func applicationDidBecomeActive(_ application: UIApplication) {}
     
     public func logout() -> Future<(), ReachFiveError> {
-        GIDSignIn.sharedInstance()?.signOut()
-        return Future.init(value: ())
+        GIDSignIn.sharedInstance.signOut()
+        return Future(value: ())
     }
     
     public override var description: String {
