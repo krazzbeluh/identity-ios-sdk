a0cf3d25a0c42cba8992a2abd445385023ba31b8
Paul LECLERC <paul.leclerc@niji.fr>
2024-07-04 11:22:12 +0200

    Feat: Add SPM support on IdentitySdkCore

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "identity-ios-sdk",
+    platforms: [.iOS(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "IdentitySdkCore",
+            targets: ["IdentitySdkCore"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.8.0")),
+        .package(url: "https://github.com/Thomvis/BrightFutures.git", .upToNextMajor(from: "8.2.0")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.8.0")),
+        .package(url: "https://github.com/devicekit/DeviceKit.git", .upToNextMajor(from: "5.1.0")),
+        .package(url: "https://github.com/facebook/facebook-ios-sdk.git", .upToNextMajor(from: "17.0.0")),
+        .package(url: "https://github.com/google/GoogleSignIn-iOS.git", .upToNextMajor(from: "7.0.0")),
+    ],
+    targets: [
+        .target(
+            name: "IdentitySdkCore",
+            dependencies: [
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "CryptoSwift", package: "CryptoSwift"),
+                .product(name: "DeviceKit", package: "DeviceKit"),
+                .product(name: "BrightFutures", package: "BrightFutures"),
+            ],
+            path: "IdentitySdkCore"),
+    ]
+)
